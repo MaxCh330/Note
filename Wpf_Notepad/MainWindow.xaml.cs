c7294ef6629f32be265f5d2ec43c369403d2530c
MaxCh330 <asdfgtrt@gmail.com>
2018-05-16 21:39:39 +0800

    finished

--- Wpf_Notepad/MainWindow.xaml.cs
+++ Wpf_Notepad/MainWindow.xaml.cs
@@ -20,88 +20,102 @@ namespace BasicWpfNotepad
     /// </summary>
     public partial class MainWindow : Window
     {
+        //定義儲存路徑
         string filePath = "";
+        //定義用作判斷是否儲存的參考值
+        string savedtext = "";
+        
 
         public MainWindow()
         {
             InitializeComponent();
         }
-        //New button
 
-        // 開啟檔案按鈕
+        // 開啟檔案按鈕Open
         private void OpenBtn_Click(object sender, RoutedEventArgs e)
         {
-            // 產生開啟檔案視窗 OpenFileDialog 
-            Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
-
-            // 顯示視窗
-            Nullable<bool> result = dlg.ShowDialog();
-
-            // 當按下開啟之後的反應 
-            if (result == true)
+            if (TextArea.Text != savedtext)
             {
-                // 取得檔案路徑 
-                filePath = dlg.FileName;
-
-                // 讀取檔案
-                TextArea.Text = System.IO.File.ReadAllText(filePath);
-            }
-        }
-
-
-        // 存檔檔案按鈕
-        private void SaveBtn_Click(object sender, RoutedEventArgs e)
-        {
-            // 產生開啟檔案視窗 OpenFileDialog 
-            Microsoft.Win32.SaveFileDialog dlg = new Microsoft.Win32.SaveFileDialog();
-
-            // 顯示視窗
-            Nullable<bool> result = dlg.ShowDialog();
-
-            // 當按下開啟之後的反應 
-            if (result == true)
-            {
-                // 取得檔案路徑 
-                filePath = dlg.FileName;
-
-                // 儲存檔案
-                System.IO.File.WriteAllText(filePath, TextArea.Text);
-            }
-        }
-        //快速存檔
-        private void QuickSaveBtn_Click(object sender, RoutedEventArgs e)
-        {
-
-        }
-        //新開檔案
-        private void NewBtn_Click(object sender, RoutedEventArgs e)
-        {
-            if (TextArea.Text != "")
-            {
-                Microsoft.Win32.SaveFileDialog dlg = new Microsoft.Win32.SaveFileDialog();
-
-                // 顯示視窗
-                Nullable<bool> result = dlg.ShowDialog();
-
-                // 當按下開啟之後的反應 
-                if (result == true)
+                if (MessageBox.Show("Do you want to Save?", "YES or NO", MessageBoxButton.YesNo) == MessageBoxResult.Yes)
                 {
-                    // 取得檔案路徑 
-                    filePath = dlg.FileName;
-
-                    // 儲存檔案
-                    System.IO.File.WriteAllText(filePath, TextArea.Text);
+                    save();
+                    open();
                 }
-                TextArea.Text = "";
+                else
+                {
+                    open();
+                }
+                
             }
             else
             {
+                open();
+            }
+            
+        }
+
+        // 存檔檔案按鈕 Save As
+        private void SaveBtn_Click(object sender, RoutedEventArgs e)
+        {
+            save();
+            savedtext = TextArea.Text;
+        }
+
+        // 快速存檔 Save
+        private void QuickSaveBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (filename.Text == filePath)
+            {
+                System.IO.File.WriteAllText(filePath, TextArea.Text);
+                savedtext = TextArea.Text;
+            }
+            else
+            {
+                save();
+                savedtext = TextArea.Text;
+            }
+            
+        }
+
+        //新開檔案 New
+        private void NewBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (savedtext != TextArea.Text)
+            {
+                if (MessageBox.Show("Do you want to Save?", "YES or NO", MessageBoxButton.YesNo) == MessageBoxResult.Yes)
+                {
+                    save();
+                }
+                else
+                {
+                    //設定案名為NEW 用作Save的判斷
+                    filename.Text = "New";
+
+                    //清空文字方格
+                    TextArea.Text = "";
+
+                    //設定判斷是否儲存過的參考值
+                    savedtext = TextArea.Text;
+                }                    
+            }
+            else
+            {
+                //設定案名為NEW 用作Save的判斷
+                filename.Text = "New";
+
+                //清空文字方格
                 TextArea.Text = "";
+
+                //設定判斷是否儲存過的參考值
+                savedtext = TextArea.Text;
             }           
         }
 
+        //黑夜模式切換
         private void DarkmodeSwitch_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
+
+            //背景與字的顏色判斷
             if (TextArea.Background == Brushes.White)
             {
                 TextArea.Foreground = Brushes.White;
@@ -122,6 +136,7 @@ namespace BasicWpfNotepad
             
         }
 
+        //字的大小切換
         private void frontSize12_Click(object sender, RoutedEventArgs e)
         {
             TextArea.FontSize = 12;
@@ -135,6 +150,108 @@ namespace BasicWpfNotepad
         private void frontSize24_Click(object sender, RoutedEventArgs e)
         {
             TextArea.FontSize = 24;
+        }
+
+        //另存共用Void
+        void save()
+        {
+            // 產生開啟檔案視窗 OpenFileDialog 
+            Microsoft.Win32.SaveFileDialog dlg = new Microsoft.Win32.SaveFileDialog();
+
+            // 顯示視窗
+            Nullable<bool> result = dlg.ShowDialog();
+
+            // 當按下開啟之後的反應 
+            if (result == true)
+            {
+                // 取得檔案路徑 
+                filePath = dlg.FileName;
+
+                // 儲存檔案
+                System.IO.File.WriteAllText(filePath, TextArea.Text);
+
+                ////設定判斷是否儲存過的參考值
+                savedtext = TextArea.Text;
+
+                //設定是否需要另存的判斷參考值
+                filename.Text = filePath;
+                
+            }
+        }
+
+        //開案共用Void
+        void open()
+        {
+            // 產生開啟檔案視窗 OpenFileDialog 
+            Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
+
+            // 顯示視窗
+            Nullable<bool> result = dlg.ShowDialog();
+
+            // 當按下開啟之後的反應 
+            if (result == true)
+            {
+                // 取得檔案路徑 
+                filePath = dlg.FileName;
+
+                // 讀取檔案
+                TextArea.Text = System.IO.File.ReadAllText(filePath);
+
+                //設定判斷是否儲存過的參考值
+                savedtext = TextArea.Text;
+
+                //設定是否需要另存的判斷參考值
+                filename.Text = filePath;
+            }
+        }
+
+        //移動視窗
+        private void TitleBar_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                this.DragMove();
+            }
+        }
+
+        //關閉視窗
+        private void stopButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (savedtext != TextArea.Text)
+            {
+                if (MessageBox.Show("Do you want to Save?", "Save or Not", MessageBoxButton.YesNo) == MessageBoxResult.Yes)
+                {
+                    save();
+                    this.Close();
+                }
+                else
+                {
+                    this.Close();
+                }
+            }
+            else
+            {
+                this.Close();
+            }
+        }
+
+        //最大化或還原
+        private void maximumButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (this.WindowState != WindowState.Maximized)
+            {
+                this.WindowState = WindowState.Maximized;
+            }
+            else
+            {
+                this.WindowState = WindowState.Normal;
+            }
+        }
+        
+        //最小化
+        private void minimunButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
         }
     }
 }
